599cfdd81cc9e95651b976a3533246e7b8273560
zys <zys@zysdeiMac.local>
2016-11-02 14:59:43 +0800

    Initial Commit

---
+++ YSWebProgressDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YSWebProgressDemo
+//
+//  Created by zys on 2016/11/2.
+//  Copyright © 2016年 张永帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YSWebProgressDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YSWebProgressDemo
+//
+//  Created by zys on 2016/11/2.
+//  Copyright © 2016年 张永帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
